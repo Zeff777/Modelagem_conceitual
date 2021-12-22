0f8fb9134f5c65b6f98f32635e9a72bb756a4f7c
Daniel Silva <danielsilvaborges777@hotmail.com>
2021-12-22 20:09:56 -0300

    Projeto criado.

---
+++ src/main/java/com/danielsilva/modelagem_dados/ModelagemDadosApplication.java
@@ -0,0 +1,13 @@
+package com.danielsilva.modelagem_dados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemDadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielsilva/modelagem_dados/ModelagemDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsilva.modelagem_dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
