110144c93425522fe95ab42d80b7ef7d8b6374d3
Daniel Silva <danielsilvaborges777@hotmail.com>
2021-12-23 17:33:01 -0300

    Criando operacao de instanciacao

--- src/main/java/com/danielsilva/modelagem_dados/ModelagemDadosApplication.java
+++ src/main/java/com/danielsilva/modelagem_dados/ModelagemDadosApplication.java
@@ -1,13 +1,32 @@
 package com.danielsilva.modelagem_dados;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemDadosApplication {
+import com.danielsilva.modelagem_dados.domain.Categoria;
+import com.danielsilva.modelagem_dados.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemDadosApplication implements CommandLineRunner {
+
+    @Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemDadosApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
