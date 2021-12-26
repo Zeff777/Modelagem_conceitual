c323e3693d90e2c984225061eaa605c9431cfffd
Daniel Silva <danielsilvaborges777@hotmail.com>
2021-12-26 15:55:34 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/danielsilva/modelagem_dados/ModelagemDadosApplication.java
+++ src/main/java/com/danielsilva/modelagem_dados/ModelagemDadosApplication.java
@@ -1,5 +1,6 @@
 package com.danielsilva.modelagem_dados;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.danielsilva.modelagem_dados.domain.Categoria;
 import com.danielsilva.modelagem_dados.domain.Cidade;
+import com.danielsilva.modelagem_dados.domain.Cliente;
+import com.danielsilva.modelagem_dados.domain.Endereco;
 import com.danielsilva.modelagem_dados.domain.Estado;
 import com.danielsilva.modelagem_dados.domain.Produto;
+import com.danielsilva.modelagem_dados.domain.enums.TipoCliente;
 import com.danielsilva.modelagem_dados.repositories.CategoriaRepository;
 import com.danielsilva.modelagem_dados.repositories.CidadeRepository;
+import com.danielsilva.modelagem_dados.repositories.ClienteRepository;
+import com.danielsilva.modelagem_dados.repositories.EnderecoRepository;
 import com.danielsilva.modelagem_dados.repositories.EstadoRepository;
 import com.danielsilva.modelagem_dados.repositories.ProdutoRepository;
 
@@ -27,6 +33,10 @@ public class ModelagemDadosApplication implements CommandLineRunner {
     private EstadoRepository estadoRepository;
     @Autowired
     private CidadeRepository cidadeRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemDadosApplication.class, args);
@@ -58,11 +68,21 @@ public class ModelagemDadosApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "3637912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("45168495", "12346890"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 307", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 777", "Centro", "38777012", cli1, c2);
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
 
 	}
 
